36b553a31b735b8fd5eb4fdd744c69736d5c74a7
Bryce McLachlan <brycemclachlan@gmail.com>
2021-05-05 20:51:46 +0000

    Updated skills, projects and copy.

--- src/data/index.js
+++ src/data/index.js
@@ -12,8 +12,37 @@ import {
 name('Bryce McLachlan')
 position('Full Stack Web Developer')
 summary(
-  `I am an experienced building designer looking to make the career shift to web development. During the last 6 months I have focused on developing my skills to become a web developer.`,
+  `I am an experienced building designer looking to make the career shift to web development. I am a quick learner and have great attention to detail which is critical in building design as well as web development.`,
 )
+project({
+  name: 'xkdc Viewer',
+  date: 'May 2021',
+  github: 'https://github.com/brycemcl/xkdc-viewer',
+  liveDemo: 'https://xkcd.brycemclachlan.me/',
+  callToAction: 'Live Demo',
+  summary:
+    'Created with Next.js, this progressive web app is statically deployed to a CDN via a CI/CD pipeline. A service worker enables offline access.',
+  stack: [
+    'React',
+    'Next.js',
+    // 'Service Worker',
+    'Tailwindcss',
+    'Github Actions',
+    // 'Firebase Hosting',
+    'Typescript',
+    // 'Git',
+  ],
+})
+project({
+  name: 'The resume you are looking at',
+  date: 'May 2021',
+  github: 'https://github.com/brycemcl/resume',
+  liveDemo: 'https://brycemcl.github.io/resume/',
+  callToAction: 'Updated PDF',
+  summary:
+    'Created with React-PDF, this resume shows how print media can be automated using web technology.',
+  stack: ['React', 'React-PDF', 'Babel', 'Font Awesome', 'Javascript', 'Git'],
+})
 project({
   name: 'Pattr.Chat',
   date: 'March 2021',
@@ -21,7 +50,7 @@ project({
   liveDemo: 'https://pattr.chat/',
   callToAction: 'Live Demo',
   summary:
-    'Created with React, this project uses GraphQL subscriptions to update multiple clients in realtime. The architecture allows for horizontal scaling and is deployed in a Kubernetes cluster with a load balancer, ingress & https certificates. Created with two other contributors we used extensive pull requests and code reviews.',
+    'Created with React, this project uses GraphQL subscriptions to update multiple clients in realtime. The architecture allows for horizontal scaling & is deployed in a Kubernetes cluster with a load balancer, ingress & https certificates. Created with two other contributors we used extensive pull requests & code reviews.',
   stack: [
     'React',
     'Apollo',
@@ -34,42 +63,29 @@ project({
     'Git',
   ],
 })
-project({
-  name: 'The resume you are looking at',
-  date: 'March 2021',
-  github: 'https://github.com/brycemcl/resume',
-  liveDemo: 'https://brycemcl.github.io/resume/',
-  callToAction: 'Updated PDF',
-  summary:
-    'Created with React-PDF, this resume shows how print media can be automated using web technology.',
-  stack: ['React', 'React-PDF', 'Babel', 'Font Awesome', 'Javascript', 'Git'],
-})
-project({
-  name: 'Scheduler',
-  date: 'March 2021',
-  github: 'https://github.com/brycemcl/scheduler',
-  // liveDemo: 'https://github.com/brycemcl/scheduler',
-  // callToAction: '',
-  summary:
-    'Created with React, this project demonstrates a modern web technology stack.',
-  stack: [
-    'React',
-    'ReactDOM',
-    // 'Kubernetes',
-    // 'Docker',
-    'PostgreSQL',
-    'Express',
-    'Rest',
-    'Create React App',
-    'Babel',
-    'Axios',
-    'Storybook',
-    'Jest',
-    'Cypress',
-    'Javascript',
-    'Git',
-  ],
-})
+
+// project({
+//   name: 'Scheduler',
+//   date: 'March 2021',
+//   github: 'https://github.com/brycemcl/scheduler',
+//   summary:
+//     'Created with React, this project demonstrates a modern web technology stack.',
+//   stack: [
+//     'React',
+//     'ReactDOM',
+//     'PostgreSQL',
+//     'Express',
+//     'Rest',
+//     'Create React App',
+//     'Babel',
+//     'Axios',
+//     'Storybook',
+//     'Jest',
+//     'Cypress',
+//     'Javascript',
+//     'Git',
+//   ],
+// })
 contactInfo({
   src: 'mailto:brycemclachlan@gmail.com',
   iconName: 'solid/envelope-square.svg',
@@ -94,7 +110,7 @@ education({
   organization: 'Lighthouse Labs',
   position: 'Web Development Diploma',
   skills:
-    'Learned practical skills to use web technologies to build modern websites, apps, and tools.',
+    'Learned practical skills to use web technologies to build modern websites, apps, & tools.',
   dateStart: '2021',
   dateEnd: '2021',
 })
@@ -102,7 +118,7 @@ education({
   organization: 'University of the Fraser Valley',
   position: 'Architectural Drafting Certificate',
   skills:
-    'Developed skills to design buildings, write technical documents, draw building plans and navigate the legal frameworks regulating the building industry.',
+    'Developed skills to design buildings, write technical documents, draw building plans & navigate the legal frameworks regulating the building industry.',
   dateStart: '2015',
   dateEnd: '2016',
 })
@@ -115,6 +131,8 @@ education({
   dateEnd: '2015',
 })
 skill({ Languages: 'Javascript' })
+skill({ Languages: 'Typescript' })
+skill({ Languages: 'Bash' })
 skill({ Languages: 'HTML' })
 skill({ Languages: 'CSS' })
 skill({ Languages: 'SQL (Postgres)' })
@@ -122,19 +140,33 @@ skill({ Languages: 'GraphQL' })
 // skill({ Languages: 'Ruby' })
 // skill({ Languages: 'Go' });
 // skill({ Languages: 'Rust' });
-// skill({ Environments: 'Virtual Machines(Hyper-V and KVM)' });
-// skill({ Environments: 'Containers(Docker and Kubernetes)' });
-// skill({ Environments: 'Bare Metal Linux (Headless/server and Desktop)' });
+skill({ Frameworks: 'Express' })
+// skill({ Frameworks: 'Ruby on Rails' })
+// skill({ Frameworks: 'React Ecosystem' })
+skill({ Frameworks: 'Next.js' })
+skill({ Frameworks: 'Tailwind CSS' })
+skill({ libraries: 'React' })
+// skill({ libraries: 'ReactDOM' });
+// skill({ libraries: 'React-PDF' });
+skill({ libraries: 'Redux' })
+skill({ libraries: 'Axios' })
+skill({ libraries: 'Apollo Client' })
+skill({ libraries: 'jQuery' })
+skill({ Environments: 'Client Side(Web Browsers)' })
+skill({ Environments: 'Serverless(Firebase Functions)' })
+skill({ Environments: 'Containers(Docker & Kubernetes)' })
+skill({ Environments: 'Virtual Machines(Hyper-V & KVM)' })
+skill({ Environments: 'Bare Metal(Server & Desktop)' })
 skill({ Storage: 'SQL (PostgreSQL)' })
 skill({ Storage: 'Object Storage (S3)' })
 skill({ Storage: 'Ceph-Rook (Block)' })
 skill({ Storage: 'NAS (Samaba running on a on-prem Kubernetes cluster)' })
-skill({ Storage: 'Nextcloud (docker-compose on a dedicated cloud server)' })
-skill({ Automation: 'Rsync' })
-skill({ Automation: 'Bash' })
-skill({ Automation: 'rsnapshot' })
-skill({ Runtimes: 'Node' })
-skill({ Runtimes: 'Browsers' })
+// skill({ Storage: 'Nextcloud (docker-compose on a dedicated cloud server)' })
+// skill({ Automation: 'Rsync' })
+// skill({ Automation: 'Bash' })
+// skill({ Automation: 'rsnapshot' })
+// skill({ Runtimes: 'Node' })
+// skill({ Runtimes: 'Browsers' })
 skill({ Tools: 'NPM' })
 skill({ Tools: 'NPX' })
 skill({ Tools: 'Babel' })
@@ -143,15 +175,16 @@ skill({ Tools: 'PostCSS' })
 skill({ Tools: 'Webpack' })
 // skill({ Tools: 'PSQL' });
 skill({ Tools: 'Docker' })
-skill({ Tools: 'Docker-Compose' })
+// skill({ Tools: 'Docker-Compose' })
 // skill({ Tools: 'Kind(Kubernetes in Docker)' });
 // skill({ Tools: 'DigitalOcean Kubernetes' });
 // skill({ Tools: 'Microk8s' });
 skill({ Tools: 'Kubectl' })
-skill({ Tools: 'Kubeadm' })
-skill({ Tools: 'Skaffold' })
+// skill({ Tools: 'Kubeadm' })
+// skill({ Tools: 'Skaffold' })
 skill({ Tools: 'Git' })
-// skill({ Tools: 'Github' });
+// skill({ Tools: 'Github' })
+skill({ Tools: 'Github Actions' })
 // skill({ Tools: 'Gitea' });
 // skill({ 'Office Tools': 'Autodesk AutoCAD' });
 // skill({ 'Office Tools': 'Autodesk Revit' });
@@ -165,42 +198,31 @@ skill({ Tools: 'Git' })
 // skill({ 'Office Tools': 'Google Meet' });
 // skill({ 'Office Tools': 'Google Docs' });
 // skill({ 'Office Tools': 'Google Sheets' });
-skill({ Frameworks: 'Express' })
-// skill({ Frameworks: 'Ruby on Rails' })
-// skill({ Frameworks: 'React Ecosystem' });
-skill({ Frameworks: 'Tailwind CSS' })
-skill({ libraries: 'React' })
-// skill({ libraries: 'ReactDOM' });
-// skill({ libraries: 'React-PDF' });
-// skill({ libraries: 'Redux' });
-skill({ libraries: 'Axios' })
-skill({ libraries: 'Apollo Client' })
-skill({ libraries: 'jQuery' })
-skill({ 'Test Tools': 'Mocha' })
-skill({ 'Test Tools': 'Chai' })
-skill({ 'Test Tools': 'Jest' })
-skill({ 'Test Tools': 'Cypress' })
+skill({ Testing: 'Mocha' })
+skill({ Testing: 'Chai' })
+skill({ Testing: 'Jest' })
+skill({ Testing: 'Cypress' })
 experience({
   // organization: 'JCR Design',
-  organization: 'JCR Design and Drafting',
+  organization: 'JCR Design & Drafting',
   position: 'Building Designer',
   dateStart: '2016',
   dateEnd: '2020',
   skills: [
     // 'System administration for internal services',
-    // 'Draw building plans in AutoCAD and Revit',
+    // 'Draw building plans in AutoCAD & Revit',
     // 'Communicate with stakeholders in project',
     // 'Review junior building designers drawings',
-    // 'Mentor and train junior building designers',
-    // 'Research and ensure compliance with zoning regulations, building bylaws and building codes',
+    // 'Mentor & train junior building designers',
+    // 'Research & ensure compliance with zoning regulations, building bylaws & building codes',
     // 'Collaborate with team members as necessary',
-    'In my position as as a building designer I automated the repetitive parts of my job and took on some system admin duties. During COVID my team had to transition from the office to WFH. During this transition I setup the IT infrastructure and documentation to make this a reality.',
+    'In my position as as a building designer I automated the repetitive parts of my job & took on some system admin duties. During COVID my team had to transition from the office to WFH. During this transition I setup the IT infrastructure & documentation to make this a reality.',
   ],
 })
 experience({
   // organization: 'The Keg',
-  organization: 'The Keg Steakhouse and Bar',
-  position: 'Preparation and 1st Cook',
+  organization: 'The Keg Steakhouse & Bar',
+  position: 'Preparation & 1st Cook',
   dateStart: '2010',
   dateEnd: '2016',
   skills: [
@@ -208,7 +230,7 @@ experience({
     // 'Worked way up to the highest positions in the kitchen',
     // 'Semi-finalist in internal Keg wide “Grill Master” competition',
     // 'Lead a team of cooks',
-    // 'Respond to guest requests and complaints',
+    // 'Respond to guest requests & complaints',
     // // 'Provide customer service over the phone',
     // 'Train staff',
 
